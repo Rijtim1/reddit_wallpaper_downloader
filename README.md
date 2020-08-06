f0d9dae3167f0249dc30092788a78eef0b7bdcfc
Rijan Timsina <regiontimsina@gmail.com>
2020-08-06 15:18:48 -0400

    added files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # reddit_wallpaper_downloader
+
+This script will download top 100 wallpapers from r/wallpaper.
+
+## Requirements
+
+The following are required. use https://pythonprogramming.net/introduction-python-reddit-api-wrapper-praw-tutorial/ to find the information on where to file the following:
+
+CLIENT_ID
+CLIENT_SECRET
+PASSWORD
+USER_AGENT
+USERNAME
--- main.py
+++ main.py
@@ -1,18 +1,47 @@
+import os
+import shutil
 import praw
+import requests
 
-CLIENT_ID = 'P6lZCiD8Bf4KAQ'
-CLIENT_SECRET = 'LAMc1VLORXN1_pDjO6nekKmdH1k'
-PASSWORD = "Sujana0512"
-USER_AGENT = "praw"
-USERNAME = "nipple_cripp"
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+PASSWORD = ""
+USER_AGENT = ""
+USERNAME = ""
 
 reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
                      username=USERNAME, password=PASSWORD, user_agent=USER_AGENT)
 
-
 subreddit = reddit.subreddit('wallpaper')
-hot_wallpaper = subreddit.hot()
+hot_wallpaper = subreddit.top("all")
+url_list = []
 
 for hot in hot_wallpaper:
-    print("Title: {} \nUps: {} \nDowns: {} \nURL: {}".format(
-        hot.title, hot.ups, hot.downs, hot.url))
+    name_url = (hot.title, hot.url)
+    url_list.append(name_url)
+
+count = 1
+image_name = []
+
+for i in range(len(url_list)):
+    response = requests.get(url_list[i][1], stream=True)
+    file_name = "images_" + str(count) + ".jpg"
+    image_name.append(file_name)
+    try:
+        file = open(file_name, "wb")
+        file.write(response.content)
+        file.close()
+    except Exception as e:
+        print(e)
+        print("Exception happened, but was caught")
+    count += 1
+
+try:
+    os.mkdir("images")
+except Exception as e:
+    print(e)
+    pass
+
+for images in image_name:
+    destination = "images/{}".format(images)
+    shutil.move("{}".format(images), destination)
